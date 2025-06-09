31c79ccdcd2889d9ccd0b3c1512df3c3fb8d43e5
RonBurgundy45 <aarondavidson88@gmail.com>
2025-06-10 00:42:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const leagueRoutes = require('./routes/league');
+app.use('/api/leagues', leagueRoutes);
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
