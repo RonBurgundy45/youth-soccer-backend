a7696520c60958b577c6f1fb8306d6145faf48f7
RonBurgundy45 <aarondavidson88@gmail.com>
2025-06-10 00:54:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const leagueRoutes = require('./routes/league');
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/leagues', leagueRoutes);
-
-
-const leagueRoutes = require('./routes/league');
 app.use('/api/leagues', leagueRoutes);
 
 // Connect to MongoDB
@@ -25,8 +20,6 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
-
-// TODO: Add routes here (I can provide these next)
 
 // Start server
 const PORT = process.env.PORT || 5000;
