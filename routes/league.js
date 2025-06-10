58e7cb865611ad18fbdb7227c3f74120f79a4b09
RonBurgundy45 <aarondavidson88@gmail.com>
2025-06-10 18:18:45 +0100

    ADD GET AND POST LOGIC

--- routes/league.js
+++ routes/league.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const League = require('../models/League');
 
-// Example route for /api/leagues
-router.get('/', (req, res) => {
-  res.json({ message: 'List of leagues' });
+// GET all leagues
+router.get('/', async (req, res) => {
+  try {
+    const leagues = await League.find();
+    res.json(leagues);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// POST create a new league
+router.post('/', async (req, res) => {
+  try {
+    const newLeague = new League({
+      name: req.body.name,
+      country: req.body.country,
+    });
+    const savedLeague = await newLeague.save();
+    res.status(201).json(savedLeague);
+  } catch (err) {
+    res.status(400).json({ error: 'Bad Request' });
+  }
 });
 
 module.exports = router;
